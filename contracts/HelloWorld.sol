d113fb91a64241b338a41e3df045e9975a7c299c
congle-engineer <congle-engineer@gmail.com>
2024-05-19 13:31:43 +0700

    Update develop

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.7.3;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
 contract HelloWorld {
     event UpdatedMessages(string oldStr, string newStr);
