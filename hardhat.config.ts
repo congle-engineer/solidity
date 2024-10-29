cc93c7ccbe6b5c4930200513acd6df83b6e79f7a
congle-engineer <congle-engineer@gmail.com>
2024-10-29 16:00:03 +0700

    Update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  networks: {
+    sepolia: {
+      url: process.env.API_URL,
+    },
+  },
 };
 
 export default config;
