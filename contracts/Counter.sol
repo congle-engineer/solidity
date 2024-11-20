aea790af890627e86d66cd6b011787dcbe835e41
congle-engineer <congle-engineer@gmail.com>
2024-11-20 11:03:40 +0700

    Add more contracts

---
+++ contracts/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Counter {
+    uint256 public count = 0;
+
+    function get() public view returns (uint256) {
+        return count;
+    }
+
+    function inc() public {
+        count += 1;
+    }
+
+    function dec() public {
+        count -= 1;
+    }
+}
