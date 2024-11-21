459e0e5a640c007354277b15a7fed23eb4de977e
congle-engineer <congle-engineer@gmail.com>
2024-11-21 17:32:33 +0700

    Add more contracts

---
+++ contracts/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Constants {
+    address public constant MY_ADDRESS =
+        0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    uint256 public constant MY_UINT = 123;
+}
